1cf7d857916898e9d8fe590f30cdc5360347874b
Felix Tamakloe <felix.tamakloe@turntabl.io>
2022-12-01 12:54:44 +0000

    Started recreating order service structure

--- services/order-service/src/main/java/org/tlc/microservices/orderservice/service/OrderProcessor.java
+++
@@ -1,8 +0,0 @@
-package org.tlc.microservices.orderservice.service;
-
-import org.tlc.microservices.orderservice.dto.OrderRequest;
-import org.tlc.microservices.orderservice.dto.Response;
-
-public interface OrderProcessor {
-    Response processOrder(OrderRequest newOrder);
-}
