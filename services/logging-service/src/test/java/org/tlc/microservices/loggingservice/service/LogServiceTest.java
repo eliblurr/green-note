ff70f2d89a7abc99f33d4600a70157bc4ec36d61
eliblurr <elvissegbawu@gmail.com>
2022-12-01 09:43:25 +0000

    initialise test

---
+++ services/logging-service/src/test/java/org/tlc/microservices/loggingservice/service/LogServiceTest.java
@@ -0,0 +1,23 @@
+package org.tlc.microservices.loggingservice.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogServiceTest {
+
+    @Test
+    void read() {
+//        mock request for expected results and check
+//        verify findByUser is called at least once
+    }
+
+    @Test
+    void create() {
+//        check if save is called
+//        check if setMessage is called
+//        check if setUser is called
+//        check if convertToEntity is called
+
+    }
+}
