fcd0d76d6ecfb81cfea2908562363fa04357f3c8
ineza743 <afsanat.ineza@ashesi.edu.gh>
2022-12-03 11:37:40 +0000

    select fields to send in RedisMessage

--- services/market-data-service/src/main/java/org/tlc/microservices/marketdataservice/controller/WebHookController.java
+++ services/market-data-service/src/main/java/org/tlc/microservices/marketdataservice/controller/WebHookController.java
@@ -4,24 +4,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.tlc.microservices.marketdataservice.model.RedisMessage;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("api/newOrder")
 public class WebHookController {
-    public static List<Object> marketDataOutput = new ArrayList<>();
-
-    public Object newObject;
-    public String getNewObject(){
-        return this.newObject.toString();
-    }
+    public static List<RedisMessage> marketDataOutput = new ArrayList<>();
 
     @PostMapping
     public void getMarketData(@RequestBody Object newData){
-        marketDataOutput.add(newData);   //add incoming orders to the list
-        this.newObject=newData;
-        System.out.println("Data came in"+ newData);
+        List<String> received= Stream.of(newData.toString())
+                .map(field -> field.split(","))
+                .flatMap(Arrays::stream)
+                .flatMap(Pattern.compile("=")::splitAsStream)
+                .collect(Collectors.toList());
+
+        RedisMessage redisMessage = new RedisMessage(received.get(3),Double.parseDouble(received.get(9)),received.get(1));
+        marketDataOutput.add(redisMessage);
+        System.out.println(redisMessage);
     }
 }
