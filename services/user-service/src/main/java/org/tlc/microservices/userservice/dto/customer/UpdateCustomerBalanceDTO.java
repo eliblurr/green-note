f798d991a6ae6aa02768a71f2e6cea20ef708e91
eliblurr <elvissegbawu@gmail.com>
2022-12-03 11:22:35 +0000

    add trigger

--- services/user-service/src/main/java/org/tlc/microservices/userservice/dto/customer/UpdateCustomerBalanceDTO.java
+++ services/user-service/src/main/java/org/tlc/microservices/userservice/dto/customer/UpdateCustomerBalanceDTO.java
@@ -11,5 +11,5 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class UpdateCustomerBalanceDTO {
-    @NonNull private Double balance;
+    @NonNull private Double balance = 0.0;
 }
