afc9bc91b54655407293aebfcc8248a2d3a3063e
Felix Tamakloe <felix.tamakloe@turntabl.io>
2022-12-05 17:33:13 +0000

    Made edits to enum classes

--- services/order-service/src/main/java/org/tlc/microservices/orderservice/dto/enums/OrderPosition.java
+++ services/order-service/src/main/java/org/tlc/microservices/orderservice/dto/enums/OrderPosition.java
@@ -3,4 +3,7 @@ package org.tlc.microservices.orderservice.dto.enums;
 public enum OrderPosition {
     SHORT,
     NORMAL
+    //use .name() to get value
+
+
 }
--- services/order-service/src/main/java/org/tlc/microservices/orderservice/dto/enums/OrderStatus.java
+++ services/order-service/src/main/java/org/tlc/microservices/orderservice/dto/enums/OrderStatus.java
@@ -1,5 +1,8 @@
 package org.tlc.microservices.orderservice.dto.enums;
 
+import lombok.ToString;
+
+@ToString
 public enum OrderStatus {
     ACCEPTED,
     REJECTED
