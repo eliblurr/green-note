d5735c7d54f49cf541b6c2c7a0ec213bfbf6bad3
Felix Tamakloe <felix.tamakloe@turntabl.io>
2022-12-07 15:56:44 +0000

    Created OrderValidator service

--- services/order-service/src/main/java/org/tlc/microservices/orderservice/services/OrderValidator.java
+++ services/order-service/src/main/java/org/tlc/microservices/orderservice/services/OrderValidator.java
@@ -8,5 +8,59 @@ import org.tlc.microservices.orderservice.Response;
 
 @Component
 public class OrderValidator {
+    private final Response VALID_ORDER = new Response(true, "order is valid");
+    private final Response INVALID_QUANTITY = new Response(false, "user does not have enough of the product in their inventory");
+    private final Response UNREASONABLE_PRICE = new Response(false, "Price set is unlikely to be accepted by exchange");
+    private final Response INSUFFICIENT_FUNDS = new Response(false, "Not enough funds in account for this transaction");
+    private final Response INVALID_REQUEST = new Response(false, "Request received was not valid");
 
+    public Response validate(OrderRequestDTO order) {
+        //make request ot user service for inventory data
+//        ClientInventoryDTO
+        if(order.getSide().equals("SELL")){
+            int numberOfProductInInventory = 0;// data received from inventory service
+            int numberOfProductsToSell = order.getQuantity();
+            if(numberOfProductInInventory<numberOfProductsToSell){
+                return INVALID_QUANTITY;
+            }else{
+                return checkIfExchangeWillAcceptOrder(order);
+            }
+        }else if (order.getSide().equals("BUY")){
+            double accountBalance = 0;
+            double orderTotal = order.getQuantity() * order.getPrice();
+            if (accountBalance < orderTotal){
+               return INSUFFICIENT_FUNDS;
+            }else{
+                return checkIfExchangeWillAcceptOrder(order);
+            }
+        }else{
+            return INVALID_REQUEST;
+        }
+    }
+
+    public Response checkIfExchangeWillAcceptOrder(OrderRequestDTO order){
+        double maxPriceShift = 0;//retrieve from market data service
+        double lastTradedPrice = 0; // retrieve from market data service
+        double askPrice = 0; // from market data service. determines the lowest price being sold at.
+        //acceptable bids lastTradedPrice for buying and askPrice for selling,
+        // +/-  maxPriceShift
+
+        if (order.getSide().equals("BUY")){
+            if(Math.abs(order.getPrice() - lastTradedPrice) < maxPriceShift ){
+                return VALID_ORDER;
+            } else{
+                return UNREASONABLE_PRICE;
+            }
+        }
+
+        if (order.getSide().equals("SELL")){
+            if(Math.abs(order.getPrice() - askPrice) <maxPriceShift){
+
+                return VALID_ORDER;
+            } else{
+                return UNREASONABLE_PRICE;
+            }
+        }
+        return INVALID_REQUEST;
+    }
 }
