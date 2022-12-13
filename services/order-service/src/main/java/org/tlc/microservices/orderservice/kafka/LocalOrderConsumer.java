8b30b34c58bfc6443a98d266fe123a0d483e0022
eliblurr <elvissegbawu@gmail.com>
2022-12-13 17:14:46 +0000

    debug

---
+++ services/order-service/src/main/java/org/tlc/microservices/orderservice/kafka/LocalOrderConsumer.java
@@ -0,0 +1,19 @@
+package org.tlc.microservices.orderservice.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import org.tlc.domain.base.kafka.KafkaConsumer;
+import org.tlc.domain.base.marketData.OrderingServiceDto;
+import org.tlc.domain.base.order.dto.SaveOrderDTO;
+
+@Service
+public class LocalOrderConsumer  implements KafkaConsumer<SaveOrderDTO> {
+
+    @Override
+    @KafkaListener(topics = "${spring.kafka.topic.order.local.create}",
+            groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(SaveOrderDTO obj) {
+        // publish order to webclient here
+    }
+
+}
